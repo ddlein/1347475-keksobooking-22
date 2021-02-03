4748baf04e557133c0fe83727d4624ccfefd8a0a
Дима <70834132+ddlein@users.noreply.github.com>
2021-02-03 19:48:21 +0300

    Исправление

--- js/main.js
+++ js/main.js
@@ -2,20 +2,20 @@ function getRandomArbitrary(min, max) {
   if (max < min || max < 0 || min < 0) {
     alert('число ДО должно быть больше, чем ОТ и они должны быть положительные')
   } else {
-    return Math.round(Math.random() * (max - min) + min);
+    return Math.floor(min + Math.random() * (max + 1 - min))
   }
 }
 
-function getRandomFixed(min, max) {
+function getRandomFixed(min, max, decimal) {
   if (max < min || max < 0 || min < 0) {
     alert('число ДО должно быть больше, чем ОТ и они должны быть положительные')
   } else {
-    return ((Math.random() * (max - min) + min)).toFixed(1);
+    return (Math.random() * (max - min) + min).toFixed(decimal);
   }
 }
 
 getRandomArbitrary(10, 100)
-getRandomFixed(10, 100)
+getRandomFixed(10, 100, 4)
 
 // console.log(a)
 // console.log(b)
