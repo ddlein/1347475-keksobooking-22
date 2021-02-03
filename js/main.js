a7d29e7e11f405f93e6e7295788cde1b495686e6
Дима <70834132+ddlein@users.noreply.github.com>
2021-02-03 22:30:50 +0300

    Переделка

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function getRandomArbitrary(min, max) {
   if (max < min || max < 0 || min < 0) {
     alert('число ДО должно быть больше, чем ОТ и они должны быть положительные')
   } else {
-    return Math.floor(min + Math.random() * (max + 1 - min))
+    return Math.floor(Math.random() * (max + 1 - min) + min)
   }
 }
 
