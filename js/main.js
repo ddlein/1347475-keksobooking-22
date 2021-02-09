915358b83c4d7c489a0f327f4da20acce8813449
Дима <70834132+ddlein@users.noreply.github.com>
2021-02-09 22:01:53 +0300

    Правки

--- js/main.js
+++ js/main.js
@@ -1,24 +1,4 @@
-let getRandomArbitrary = (min, max) => {
-  if (max < min || max < 0 || min < 0) {
-    alert('число ДО должно быть больше, чем ОТ и они должны быть положительные')
-  } else {
-    return Math.floor(Math.random() * (max + 1 - min) + min)
-  }
-}
-
-let getRandomFixed = (min, max, decimal) => {
-  if (max < min || max < 0 || min < 0) {
-    alert('число ДО должно быть больше, чем ОТ и они должны быть положительные')
-  } else {
-    return (Math.random() * (max - min) + min).toFixed(decimal);
-  }
-}
-
-getRandomArbitrary(10, 100)
-getRandomFixed(10, 100, 4)
-
-// console.log(a)
-// console.log(b)
+'use strict'
 
 const TYPE = [
   'palace',
@@ -50,25 +30,56 @@ const PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
 ];
 
+const MIN_PRICE = 100;
+const MAX_PRICE = 4000;
+
+const MIN_ROOMS = 1;
+const MAX_ROOMS = 4;
+
+const MIN_GUESTS = 1;
+const MAX_GUESTS = 4;
+
 const FROM_X = 35.65000;
 const TO_X = 35.70000;
 
 const FROM_Y = 139.70000;
 const TO_Y = 139.80000;
-
-// const Y = [
-//   139.70000,
-//   139.80000,
-// ]
+const DECIMAL = 5;
+const SIMILAR_PROMO_COUNT = 10;
 
 
+let getRandomArbitrary = (min, max) => {
+  if (max < min || max < 0 || min < 0) {
+    alert('число ДО должно быть больше, чем ОТ и они должны быть положительные')
+  } else {
+    return Math.floor(Math.random() * (max + 1 - min) + min)
+  }
+}
+
+let getRandomFixed = (min, max, decimal) => {
+  if (max < min || max < 0 || min < 0) {
+    alert('число ДО должно быть больше, чем ОТ и они должны быть положительные')
+  } else {
+    return (Math.random() * (max - min) + min).toFixed(decimal);
+  }
+}
+
+let getShuffle = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array.slice(getRandomArbitrary(1, array.length-1))
+}
 
 const createPromo = () => {
-  let valueX = getRandomFixed(FROM_X, TO_X, 5);
-  let valueY = getRandomFixed(FROM_Y, TO_Y, 5);
-  let getRandomFeatures = (elements) => {
-    return elements[getRandomArbitrary(0, elements.length - 1)]
-  }
+  let valueX = getRandomFixed(FROM_X, TO_X, DECIMAL);
+  let valueY = getRandomFixed(FROM_Y, TO_Y, DECIMAL);
+  // let getRandomFeatures = (elements) => {
+  //   return elements[getRandomArbitrary(0, elements.length - 1)]
+  // }
 
   return {
     author: {
@@ -77,15 +88,17 @@ const createPromo = () => {
     offer: {
       title: 'Promo',
       address: valueX + ', ' + valueY,
-      price: getRandomArbitrary(100, 4000),
+      price: getRandomArbitrary(MIN_PRICE, MAX_PRICE),
       type: TYPE[getRandomArbitrary(0, TYPE.length - 1)],
-      rooms: getRandomArbitrary(1, 4),
-      guests: getRandomArbitrary(1, 4),
+      rooms: getRandomArbitrary(MIN_ROOMS, MAX_ROOMS),
+      guests: getRandomArbitrary(MIN_GUESTS, MAX_GUESTS),
       checkin: CHECKIN[getRandomArbitrary(0, CHECKIN.length - 1)],
       checkout: CHECKOUT[getRandomArbitrary(0, CHECKOUT.length - 1)],
-      features: new Array(getRandomArbitrary(1, FEATURES.length-1)).fill(null).map(() => getRandomFeatures(FEATURES)),
+      // features: new Array(getRandomArbitrary(1, FEATURES.length - 1)).fill(null).map(() => getRandomFeatures(FEATURES)),
+      features: getShuffle(FEATURES),
       description: 'Супер-предложение',
-      photos: new Array(getRandomArbitrary(1, PHOTOS.length-1)).fill(null).map(() => getRandomFeatures(PHOTOS)),
+      // photos: new Array(getRandomArbitrary(1, PHOTOS.length - 1)).fill(null).map(() => getRandomFeatures(PHOTOS)),
+      photos: getShuffle(PHOTOS),
     },
     location: {
       x: valueX,
@@ -94,11 +107,12 @@ const createPromo = () => {
   }
 }
 
-const SIMILAR_PROMO_COUNT = 10;
-
-const SIMILAR_PROMO = new Array(SIMILAR_PROMO_COUNT).fill(null).map(() => createPromo())
 
 
-SIMILAR_PROMO
 
-// console.log(SIMILAR_PROMO);
+const similarPromo = new Array(SIMILAR_PROMO_COUNT).fill(null).map(() => createPromo())
+
+
+similarPromo
+
+// console.log(similarPromo);
