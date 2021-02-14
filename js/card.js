93766c1ecb34a634ef103f0682573366251619ac
Дима <70834132+ddlein@users.noreply.github.com>
2021-02-14 12:12:14 +0300

    Правка

--- js/card.js
+++ js/card.js
@@ -1,39 +1,31 @@
-import {
-  similarPromo
-} from './data.js';
+import {similarPromo} from './data.js';
 
 // console.log(similarPromo);
 // console.log(createPromo);
 
 const similarListElement = document.querySelector('.map__canvas');
-const similarPromoTemplate = document
-  .querySelector('#card')
-  .content.querySelector('.popup');
+const similarPromoTemplate = document.querySelector('#card').content.querySelector('.popup');
+
+const getOfferType = (promoType) => {
+  switch (promoType) {
+    case 'flat':
+      return 'Квартира';
+    case 'bungalow':
+      return 'Бунгало';
+    case 'house':
+      return 'Дом';
+    case 'palace':
+      return 'Дворец';
+  }
+}
 
 similarPromo.forEach((promo) => {
   const promoElement = similarPromoTemplate.cloneNode(true);
   promoElement.querySelector('.popup__title').textContent = promo.offer.title;
-  promoElement.querySelector('.popup__text--address').textContent =
-    promo.offer.address;
-  promoElement.querySelector('.popup__text--price').textContent =
-    promo.offer.price + '₽/ночь';
-  const offerType = promo.offer.type;
-  let popupType;
-  switch (offerType) {
-    case 'flat':
-      popupType = 'Квартира';
-      break;
-    case 'bungalow':
-      popupType = 'Бунгало';
-      break;
-    case 'house':
-      popupType = 'Дом';
-      break;
-    case 'palace':
-      popupType = 'Дворец';
-      break;
-  }
-  promoElement.querySelector('.popup__type').textContent = popupType;
+  promoElement.querySelector('.popup__text--address').textContent = promo.offer.address;
+  promoElement.querySelector('.popup__text--price').textContent = promo.offer.price + ' ₽/ночь';
+
+  promoElement.querySelector('.popup__type').textContent = getOfferType(promo.offer.type);
 
   promoElement.querySelector('.popup__text--capacity').textContent =
     `${promo.offer.rooms} комнаты для ${promo.offer.guests} гостей`;
@@ -46,38 +38,14 @@ similarPromo.forEach((promo) => {
   for (let i = 0; i < promo.offer.features.length; i++) {
     let li = document.createElement('li');
     li.classList.add('popup__feature');
-    switch (promo.offer.features[i]) {
-      case 'wifi':
-        li.classList.add('popup__feature--wifi');
-        break;
-      case 'dishwasher':
-        li.classList.add('popup__feature--dishwasher');
-        break;
-      case 'parking':
-        li.classList.add('popup__feature--parking');
-        break;
-      case 'washer':
-        li.classList.add('popup__feature--washer');
-        break;
-      case 'elevator':
-        li.classList.add('popup__feature--elevator');
-        break;
-      case 'conditioner':
-        li.classList.add('popup__feature--conditioner');
-        break;
-    }
-    li.textContent = promo.offer.features[i]
-    ul.appendChild(li)
+    li.classList.add('popup__feature--' + promo.offer.features[i]);
+    li.textContent = promo.offer.features[i];
+    ul.appendChild(li);
   }
-
-
 
   let divPhotos = promoElement.querySelector('.popup__photos');
   let popupPhoto = promoElement.querySelector('.popup__photo');
-
   divPhotos.innerHTML = '';
-
-
   for (let i = 0; i < promo.offer.photos.length; i++) {
     popupPhoto.src = promo.offer.photos[i];
     divPhotos.appendChild(popupPhoto.cloneNode(true))
